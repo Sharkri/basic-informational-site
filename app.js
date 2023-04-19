a72efff045231b44476668e3a8699e643635e3a3
Sharkri <sharkdude955@gmail.com>
2023-04-19 00:25:49 -0500

    Refactor to use express

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const port = 3000;
 
+function getFilePath(fileName) {
+  return path.join(__dirname, fileName);
+}
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(getFilePath("index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(getFilePath("about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(getFilePath("contact-me.html"));
+});
+
+// 404 page
+app.get("*", (req, res) => {
+  res.status(404).sendFile(getFilePath("404.html"));
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
